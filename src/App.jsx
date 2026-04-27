549076fd54bab7e939c4c467efe37d37368646dd
flower <nagmanayak166@gmail.com>
2026-04-27 19:57:12 +0530

    Add alert on button click and update heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 export default function App() {
+  const handleClick = () => {
+    alert("Welcome to Paradise Nursery!");
+  };
+
   return (
     <div>
-      <h1>Paradise Nursery</h1>
-      <button>Get Started</button>
+      <h1>Welcome to Paradise Nursery</h1>
+      <button onClick={handleClick}>Get Started</button>
     </div>
   );
 }
