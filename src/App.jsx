879b546dbadc26dc90845cca8243cf62e5ca88b0
flower <nagmanayak166@gmail.com>
2026-04-27 21:15:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
+import ProductList from "./components/ProductList";
+
 export default function App() {
-  const handleClick = () => {
-    alert("Welcome to Paradise Nursery!");
-  };
+  const [showProducts, setShowProducts] = useState(false);
 
   return (
     <div>
-      <h1>Welcome to Paradise Nursery</h1>
-      <button onClick={handleClick}>Get Started</button>
+      {!showProducts ? (
+        <div>
+          <h1>Welcome to Paradise Nursery</h1>
+          <button onClick={() => setShowProducts(true)}>
+            Get Started
+          </button>
+        </div>
+      ) : (
+        <ProductList />
+      )}
     </div>
   );
 }
